3ce1c506ce2e1c386e2ab261c4a1ee1a58ada105
Vincent Volkmar <vincent.volkmar@assfinet.de>
2023-08-28 10:37:08 +0200

    Added Removing and Adding of Items using TextInput

--- ToDoList/Form1.Designer.cs
+++ ToDoList/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ToDoList
+﻿using System.Drawing;
+
+namespace ToDoList
 {
     partial class Form1
     {
@@ -28,45 +30,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.add_btn = new System.Windows.Forms.Button();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.rem_btn = new System.Windows.Forms.Button();
+            this.urgency_btn = new System.Windows.Forms.Button();
+            this.edit_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // add_btn
             // 
-            this.button1.Location = new System.Drawing.Point(376, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 66);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.add_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(219)))), ((int)(((byte)(251)))));
+            this.add_btn.FlatAppearance.BorderSize = 0;
+            this.add_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(190)))), ((int)(((byte)(248)))));
+            this.add_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.add_btn.Location = new System.Drawing.Point(492, 37);
+            this.add_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.add_btn.Name = "add_btn";
+            this.add_btn.Size = new System.Drawing.Size(128, 61);
+            this.add_btn.TabIndex = 0;
+            this.add_btn.Text = "Add Entry";
+            this.add_btn.UseVisualStyleBackColor = false;
+            this.add_btn.Click += new System.EventHandler(this.add_btn_Click);
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(219)))), ((int)(((byte)(251)))));
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Location = new System.Drawing.Point(18, 17);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(0);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(334, 634);
+            this.checkedListBox1.Size = new System.Drawing.Size(420, 760);
             this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
+            // 
+            // rem_btn
+            // 
+            this.rem_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(219)))), ((int)(((byte)(251)))));
+            this.rem_btn.FlatAppearance.BorderSize = 0;
+            this.rem_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(190)))), ((int)(((byte)(248)))));
+            this.rem_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rem_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.rem_btn.Location = new System.Drawing.Point(492, 120);
+            this.rem_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.rem_btn.Name = "rem_btn";
+            this.rem_btn.Size = new System.Drawing.Size(128, 61);
+            this.rem_btn.TabIndex = 2;
+            this.rem_btn.Text = "Remove Entry";
+            this.rem_btn.UseVisualStyleBackColor = false;
+            this.rem_btn.Click += new System.EventHandler(this.rem_btn_Click);
+            // 
+            // urgency_btn
+            // 
+            this.urgency_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(219)))), ((int)(((byte)(251)))));
+            this.urgency_btn.FlatAppearance.BorderSize = 0;
+            this.urgency_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(190)))), ((int)(((byte)(248)))));
+            this.urgency_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.urgency_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.urgency_btn.Location = new System.Drawing.Point(492, 286);
+            this.urgency_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.urgency_btn.Name = "urgency_btn";
+            this.urgency_btn.Size = new System.Drawing.Size(128, 61);
+            this.urgency_btn.TabIndex = 3;
+            this.urgency_btn.Text = "Change Importance";
+            this.urgency_btn.UseVisualStyleBackColor = false;
+            // 
+            // edit_btn
+            // 
+            this.edit_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(219)))), ((int)(((byte)(251)))));
+            this.edit_btn.FlatAppearance.BorderSize = 0;
+            this.edit_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(190)))), ((int)(((byte)(248)))));
+            this.edit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.edit_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.edit_btn.Location = new System.Drawing.Point(492, 203);
+            this.edit_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.edit_btn.Name = "edit_btn";
+            this.edit_btn.Size = new System.Drawing.Size(128, 61);
+            this.edit_btn.TabIndex = 4;
+            this.edit_btn.Text = "Edit";
+            this.edit_btn.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 664);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(160)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(650, 800);
+            this.Controls.Add(this.edit_btn);
+            this.Controls.Add(this.urgency_btn);
+            this.Controls.Add(this.rem_btn);
             this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.add_btn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1050, 1385);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(30, 83, 30, 28);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.MenuText;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button add_btn;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button rem_btn;
+        private System.Windows.Forms.Button urgency_btn;
+        private System.Windows.Forms.Button edit_btn;
     }
 }
 
--- ToDoList/Form1.cs
+++ ToDoList/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MetroFramework;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,20 +8,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
+using MetroFramework.Forms;
+using Microsoft.VisualBasic;
 
 namespace ToDoList
 {
     public partial class Form1 : Form
     {
+        private TextBox textBox1;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
+        public static string CustomTextDialog(string title, string prompt, ref string value)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = prompt;
+            textBox.Text = value;
+
+            buttonOk.Text = "Add";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.AutoSize = true;
+            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox.Location = new Point(20, 20);
+            label.Location = new Point(20, 5);
+            buttonOk.Location = new Point(100, 50);
+            buttonCancel.Location = new Point(50, 50);
+
+            form.ClientSize = new Size(200, 100);
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult result = form.ShowDialog();
+            value = textBox.Text;
+            return Convert.ToString(value);
+        }
+
+        private void add_btn_Click(object sender, EventArgs e)
+        {
+            string value = "very important thing";
+            var textValue = CustomTextDialog("Add Item", "Input To-Do Text", ref value);
+            checkedListBox1.Items.Add(textValue);
+        }
+
+        private void rem_btn_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Remove(checkedListBox1.SelectedItem);
+        }
     }
 }
